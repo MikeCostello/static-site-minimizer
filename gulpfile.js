c86dcb14763e2453b470d6b01f399dd144e3b14c
Mike Costello <mike@netobjects.com>
2015-05-01 11:57:08 -0400

    add run-sequence & gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var cssMinifer = require('gulp-minify-css');
 var imgMinifer = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var cache = require('gulp-cached');
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
 
 var paths = {
   src: 'src/**/*',
@@ -15,12 +17,17 @@ var paths = {
   images: ['src/**/*.png', 'src/**/*.jpg', 'src/**/*.gif']
 };
 
+gulp.task('clean', function() {
+  return gulp.src(paths.dist, {read: false})
+    .pipe(clean());
+});
+
 gulp.task('copy', function() {
   return gulp.src(paths.src)
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('html', ['copy'], function() {
+gulp.task('html', function() {
   return gulp.src(paths.html)
     .pipe(cache('html'))
     .pipe(htmlMinifer({
@@ -32,21 +39,21 @@ gulp.task('html', ['copy'], function() {
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('js', ['copy'], function() {
+gulp.task('js', function() {
   return gulp.src(paths.js)
     .pipe(cache('js'))
     .pipe(jsMinifer())
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('css', ['copy'], function() {
+gulp.task('css', function() {
   return gulp.src(paths.css)
     .pipe(cache('css'))
     .pipe(cssMinifer({compatibility: 'ie8'}))
     .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('images', ['copy'], function () {
+gulp.task('images', function () {
   return gulp.src(paths.images)
     .pipe(cache('images'))
     .pipe(imgMinifer({
@@ -63,5 +70,8 @@ gulp.task('watch', function() {
   gulp.watch(paths.images, ['images']);
 });
 
-gulp.task('build', ['html', 'css', 'js', 'images']);
+gulp.task('build', function(callback) {
+  runSequence('clean', 'copy', ['html', 'css', 'js', 'images'], callback);
+});
+
 gulp.task('default', ['watch', 'build']);
